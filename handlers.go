e289038dff9d92e2c7d4e54535ce782a205eaf69
Atviksord <Atviksord@gmail.com>
2024-08-01 00:15:14 +0900

    update

--- handlers.go
+++ handlers.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"time"
+
+	"github.com/Atviksord/BlogAggregator/internal/database"
+	"github.com/google/uuid"
 )
+
+// struct to create user
+type createUserResponse struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+}
 
 func ReadynessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -15,10 +29,44 @@ func ErrorHandler(w http.ResponseWriter, r *http.Request) {
 
 	respondWithError(w, 500, map[string]string{"error": "Internal server Error"})
 }
-func UserCreateHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) UserCreateHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
+	var params struct {
+		Name string `json:"name"`
+	}
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to read request body", http.StatusBadRequest)
+		return
+	}
 
-	respondWithError(w, 500, map[string]string{"error": "Internal server Error"})
+	err = json.Unmarshal(requestBody, &params)
+	if err != nil {
+		fmt.Errorf("Couldnt unmarshal r body into user struct %v", err)
+	}
+	USER, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
+	response := createUserResponse{
+		ID:        USER.ID,
+		CreatedAt: USER.CreatedAt,
+		UpdatedAt: USER.UpdatedAt,
+		Name:      USER.Name,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "Failed to write the response", http.StatusInternalServerError)
+	}
+
 }
 
 func HandlerRegistry(mux *http.ServeMux) {
@@ -26,5 +74,5 @@ func HandlerRegistry(mux *http.ServeMux) {
 	// TEST HANDLER
 	mux.HandleFunc("GET /v1/healthz", ReadynessHandler)
 	mux.HandleFunc("GET /v1/err", ErrorHandler)
-	mux.HandleFunc("POST /v1/users", UserCreateHandler)
+	mux.HandleFunc("POST /v1/users", config.UserCreateHandler)
 }
